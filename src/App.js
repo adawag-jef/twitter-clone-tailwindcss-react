afaeb0b32d55ea03e3f3dd4e17c602bc4bd937d0
Jeffrey Adawag <jeffrey@tirsolutions.com>
2020-09-14 10:16:02 +0800

    Initial react- tailwind setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/main.css";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1>Hello Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
